8647976f2d8a38c8a6d194352341395a23ca254c
Diogo Tavares <diogo.martins.tavares27@gmail.com>
2023-10-30 14:32:34 +0000

    small correction

--- webserv/srcs/main.cpp
+++ webserv/srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: diogmart <diogmart@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/30 14:27:49 by diogmart          #+#    #+#             */
-/*   Updated: 2023/10/30 14:29:29 by diogmart         ###   ########.fr       */
+/*   Updated: 2023/10/30 14:31:50 by diogmart         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 int main(int argc, char **argv) {
     
     if (argc != 2) {
-        std::cout << "Error: wrong number of arguments, it should be \"./webserv [configuration file]\"" << std::endl;
+        std::cout << "Error: wrong number of arguments.\nTry: \"./webserv [configuration file]\"" << std::endl;
         return (1);
     }
 
