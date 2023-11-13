bab7f46bd0eb29a1ff414185688d4332bb7afb42
Kelvin Faustino <kfaustin@student.42porto.com>
2023-11-13 16:04:15 +0000

    wserver

---
+++ learning/wserver/http_tcpServer_linux.hpp
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   http_tcpServer_linux.hpp                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kfaustin <kfaustin@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/13 15:46:45 by kfaustin          #+#    #+#             */
+/*   Updated: 2023/11/13 15:46:45 by kfaustin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+#ifndef HTTP_TCP_SERVER_LINUX
+# deinfe HTTP_TCP_SERVER_LINUX
+
+class TcpServer {
+	private:
+	public:
+		TcpServer(void);
+		~TcpServer(void);
+};
+
+#endif //HTTP_TCP_SERVER_LINUX
